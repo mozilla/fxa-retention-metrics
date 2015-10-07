3f70353b12a2d608d59f2abc624545048559db6f
Vlad Filippov <vlad.filippov@gmail.com>
2015-10-06 23:52:52 -0400

    feat(metrics): add more retention graphs

---
+++ books/retention_accounts_created.py
@@ -0,0 +1,28 @@
+import os, sys
+sys.path.append(os.path.realpath(os.curdir))
+from tools.convert_to_book import ConvertToBook
+
+# dev event storage
+books_dir = os.path.dirname(os.path.abspath(__file__))
+dev_event_path = os.path.join(books_dir, '..', 'tools', 'out')
+
+# prod event storage
+prod_event_path = 's3n://net-mozaws-prod-us-west-2-pipeline-analysis/fxa-retention/data/'
+
+dev_book = ConvertToBook(
+    script_file=os.path.join(books_dir, '..', 'metrics', 'retention_account_created.py'),
+    book_title='User Retention using account.created events DEVELOPMENT',
+    book_target_path=os.path.join(books_dir, '..', 'ipynb', 'dev'),
+    event_storage=dev_event_path
+)
+
+dev_book.write_book()
+
+prod_book = ConvertToBook(
+    script_file=os.path.join(books_dir, '..', 'metrics', 'retention_account_created.py'),
+    book_title='User Retention using account.created events',
+    book_target_path=os.path.join(books_dir, '..', 'ipynb', 'prod'),
+    event_storage=prod_event_path
+)
+
+prod_book.write_book()
--- tests/playground/seaborn_plot.py
+++ tests/playground/seaborn_plot.py
@@ -1,34 +1,9 @@
 """
 This is a plotting playground
 """
-# plot = [
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     [557, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 98],
-#     # [548, 100, 100, 100, 100, 100, 100, 100, 100, 100, 99],
-#     # [554, 100, 100, 100, 100, 100, 100, 100, 100, 99],
-#     # [539, 100, 100, 100, 100, 100, 100, 100, 99],
-#     # [556, 100, 100, 100, 100, 100, 100, 99], [542, 100, 100, 100, 100, 100, 99], [555, 100, 100, 100, 100, 99],
-#     #     [560, 100, 100, 100, 99], [553, 100, 100, 99], [549, 100, 99], [544, 99], [520]
-#     ]
-# import matplotlib.pyplot as plt
-# import numpy as np;
-#
-# np.random.seed(0)
-# import seaborn as sns;
-#
-# sns.set()
-# plt.figure(figsize=(10, 10))
-# plt.title('User Retention based on "account.signed"')
-# ax = sns.heatmap(plot, annot=True, fmt='d')
-# plt.show()
 
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import seaborn as sns
+import seaborn
 import pandas as pd
 from datetime import date, timedelta
 
@@ -44,14 +19,14 @@ print dates
 
 df = pd.DataFrame(data, index=dates)
 
-sns.set(style='white')
+seaborn.set(style='white')
 plt.figure(figsize=(14, 12))
 plt.xlabel('time')
 plt.ylabel('s1 and s2')
 plt.grid(True)
 
 plt.title('User Retention based on "account.signed"')
-sns.heatmap(df, annot=True, fmt='d', yticklabels=dates)
+seaborn.heatmap(df, annot=True, fmt='d', yticklabels=dates)
 locs, labels = plt.yticks()
 
 plt.setp(labels, rotation=0)
